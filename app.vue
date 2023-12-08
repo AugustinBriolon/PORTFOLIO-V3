b09d6067f1056608c6eddec5e24f7b608df9b531
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-12-08 18:41:17 +0100

    :sparkles: config rpoject with contentful data

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+@import '@/scss/main.scss';
+</style>
+
+<script setup>
+const { $client } = useNuxtApp();
+
+const fetchProjects = async ($client) => {
+  try {
+    const data = await $client.getEntries()
+    const formattedData = data.items.map((item) => ({
+      title: item.fields.title,
+    }));
+    return formattedData;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+fetchProjects($client).then((projets) => {
+  useProjects().value = projets;
+});
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    // PROD
+    // ssr: true,
+    // DEV
+    ssr: false,
+  devtools: { enabled: true },
+  css: ['@/css/main.css'],
+  vite: {
+    resolve: {
+      alias: {
+        '~': '/public',
+        '@': '/assets',
+      },
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
